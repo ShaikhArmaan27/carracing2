fadb06025e9829d74cb6e9a0626215d3492c4e0a
Shaikh Armaan <skmdarmaan27@gmail.com>
2020-12-11 08:18:18 +0530

    cars

--- js/Game.js
+++ js/Game.js
@@ -29,13 +29,13 @@ class Game {
       form.display();
     }
 
-    car1 = createSprite(100,200);
+    car1 = createSprite(displayWidth/5,200);
     car1.addImage("car1",car1_img);
-    car2 = createSprite(300,200);
+    car2 = createSprite(2*displayWidth/5,200);
     car2.addImage("car2",car2_img);
-    car3 = createSprite(500,200);
+    car3 = createSprite(3*displayWidth/5,200);
     car3.addImage("car3",car3_img);
-    car4 = createSprite(700,200);
+    car4 = createSprite(4*displayWidth/5,200);
     car4.addImage("car4",car4_img);
     cars = [car1, car2, car3, car4];
   }
@@ -55,7 +55,7 @@ class Game {
       var index = 0;
 
       //x and y position of the cars
-      var x = 175 ;
+      var x = 220 ;
       var y;
 
       for(var plr in allPlayers){
@@ -63,13 +63,15 @@ class Game {
         index = index + 1 ;
 
         //position the cars a little away from each other in x direction
-        x = x + 200;
+        x = x + 300
         //use data form the database to display the cars in y direction
         y = displayHeight - allPlayers[plr].distance;
         cars[index-1].x = x;
         cars[index-1].y = y;
 
         if (index === player.index){
+          fill("red")
+          ellipse(x,y,70,70)
           cars[index - 1].shapeColor = "red";
           camera.position.x = displayWidth/2;
           camera.position.y = cars[index-1].y;
@@ -86,7 +88,7 @@ class Game {
       player.update();
     }
 
-    if(player.distance > 3860){
+    if(player.distance > 4800){
       gameState = 2;
     }
    
